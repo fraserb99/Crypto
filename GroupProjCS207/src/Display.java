2b98f905ed0782b0365d065c5a4ee6004cd662a7
Frase <Frase@DESKTOP-RDP3TKS>
2019-01-25 14:40:24 +0000

    jasnkj

--- GroupProjCS207/src/Display.java
+++ GroupProjCS207/src/Display.java
@@ -1,3 +1,4 @@
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Insets;
@@ -8,19 +9,17 @@ import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import javafx.scene.layout.Border;
 
 public class Display extends JFrame implements ActionListener{
 
 	private ArrayList<JTextField> textArray = new ArrayList<JTextField>();
 	private String sentence = "The quick brown fox jumped over the lazy dog";
 	private String[] parsedSentence;
-	textPane.addKeyListener(new KeyAdapter() {
-		@Override
-		public void keyTyped(KeyEvent arg0) {
-		}
-	});
 	
 	public Display() {
 		setTitle("Test GUI");
@@ -30,22 +29,38 @@ public class Display extends JFrame implements ActionListener{
 		
 		for (int i=0;i<parsedSentence.length;i++) {
 			for (int c=0;c<parsedSentence[i].length();c++) {
-				JTextField temp = new JTextField(parsedSentence[i].substring(c, c+1));
+				JPanel panel = new JPanel(new BorderLayout());
+				panel.setSize(new Dimension(40,80));
+				JTextField temp = new JTextField(1);
+				JLabel label = new JLabel();
+				
+				//label.setLabelFor(temp);
+				label.setText(parsedSentence[i].substring(c, c+1));
+				temp.setVisible(true);
+				label.setVisible(true);
+				temp.addKeyListener(new KeyAdapter() {
+					@Override
+					public void keyTyped(KeyEvent arg0) {
+					}
+				});
 				if (c==0 && i!=0) {
 					JPanel separator = new JPanel();
-					separator.setSize(new Dimension(5, 15));
+					separator.setSize(new Dimension(5, 25));
 					add(separator);
 				}
 				temp.setMargin(new Insets(5,5,5,5));
 				textArray.add(temp);
-				add(temp);
+				panel.add(temp, BorderLayout.NORTH);
+				panel.add(label, BorderLayout.CENTER);
+				add(panel);
+				setVisible(true);
 			}
 			
 		}
 		
 		
 		
-		setSize(1000, 1200);
+		setSize(800, 800);
 		setVisible(true);
 	}
 	
@@ -59,9 +74,11 @@ public class Display extends JFrame implements ActionListener{
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
+	public void actionPerformed(ActionEvent e) {
 		
 		
 	}
+	
+		
 
 }
